6eeccd414de0e0eff65eae044cdcf261b4aae0e1
andrewboudreau <andrewboudreau@gmail.com>
2015-10-12 22:48:42 -0500

    added coordinator nuget hosted on appveyor

--- ShortUrl/Controllers/HomeController.cs
+++ ShortUrl/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Coordinator.Client;
 using ShortUrl.Services;
 
 namespace ShortUrl.Controllers
@@ -9,13 +11,31 @@ namespace ShortUrl.Controllers
     {
         private readonly IShortUrlService service;
 
-        public HomeController(IShortUrlService service)
+        private readonly CoordinatorClient coordinator;
+
+        public HomeController(IShortUrlService service, CoordinatorClient coordinator)
         {
             this.service = service;
+            this.coordinator = coordinator;
         }
 
         public async Task<ActionResult> Index()
         {
+            if (Request.Url == null)
+            {
+                throw new NullReferenceException("Request.Url cannot be null");
+            }
+
+            ViewBag.RegisteredSites = await coordinator.RegisterSiteAsync(new Coordinator.Models.Site()
+            {
+                Id = "shorturl",
+                Name = "ShortUrl",
+                Url =
+                    Request.Url.Scheme + Uri.SchemeDelimiter + Request.Url.Host +
+                    (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port)
+
+            });
+
             ViewBag.Recent = await service.RecentShortenedUrls().ToListAsync();
             return View();
         }
